42ac99f3671f227553da481185cb49f879a2abee
heec.choi <ss01003@naver.com>
2025-08-22 10:58:26 +0900

    Apply corner rectangle

--- dataset/canvas/component/button.py
+++ dataset/canvas/component/button.py
@@ -12,7 +12,33 @@ class ButtonComponent(UIComponentBase):
         side = min(self.w, self.h)
         cx = self.x + (self.w - side) // 2
         cy = self.y + (self.h - side) // 2
-        center = (cx + side // 2, cy + side // 2)
         radius = side // 2
-        cv2.circle(img, center, radius, color, -1)
+        self.draw_rounded_rectangle(img, radius, color)
         center_text(img, self.cls_name, cx, cy, side, side, font_scale=0.3)
+
+
+    def draw_rounded_rectangle(self, img, radius, color):
+        """
+        radius 적용된 사각형을 그려주는 함수
+        img : 이미지
+        x, y : 좌상단 좌표
+        w, h : 너비, 높이
+        radius : 모서리 둥글기
+        color : (B, G, R)
+        """
+        x = self.x
+        y = self.y
+        w = self.w
+        h = self.h
+        # radius가 너무 크면 제한
+        radius = min(radius, w // 2, h // 2)
+
+        # 직사각형 본체
+        cv2.rectangle(img, (x + radius, y), (x + w - radius, y + h), color, -1)
+        cv2.rectangle(img, (x, y + radius), (x + w, y + h - radius), color, -1)
+
+        # 모서리 원
+        cv2.circle(img, (x + radius, y + radius), radius, color, -1)  # 좌상
+        cv2.circle(img, (x + w - radius, y + radius), radius, color, -1)  # 우상
+        cv2.circle(img, (x + radius, y + h - radius), radius, color, -1)  # 좌하
+        cv2.circle(img, (x + w - radius, y + h - radius), radius, color, -1)
